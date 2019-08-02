aeea81e6a26f135d4936be81d92da2923e94f5dc
tama <tama@ttk1.net>
2019-08-02 22:41:48 +0900

    [#3] texelFetch版

--- src/glsl/2/vert.glsl
+++ src/glsl/2/vert.glsl
@@ -2,5 +2,5 @@
 out float result;
 uniform sampler2D tex;
 void main(void) {
-    result = texture(tex, vec2(float(gl_VertexID) / 4.0 + 0.125, 0.5)).x;
+    result = texelFetch(tex, ivec2(gl_VertexID, 0), 0).x;
 }
